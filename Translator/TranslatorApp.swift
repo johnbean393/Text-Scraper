e21c62d04c4a9b2585b1897870fdf3bf3eeccfca
johnbean393 <johnbean393@gmail.com>
2024-08-07 23:44:42 +0800

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Bean John on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
